c0942a2effc82dc6102cf208e6552af7cd025d49
GAI-313 <nakatogawagai@gmail.com>
2024-09-17 18:44:38 +0900

    Add some configurations

--- erasers_kachaka/erasers_kachaka_bringup/launch/bringup.launch.py
+++ erasers_kachaka/erasers_kachaka_bringup/launch/bringup.launch.py
@@ -1,20 +1,39 @@
 #!/usr/bin/env python3
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, ExecuteProcess
+from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition
 
 from ament_index_python.packages import get_package_share_directory
 
+import os
 
 def generate_launch_description():
     ld = LaunchDescription()
+
+    # launch configurations
+    LaunchConfiguration("provide_map")
+    LaunchConfiguration("show_rviz")
 
     # map
     map2odom_pose = ["0", "0", "0", "0", "0", "0", "map", "odom"]
 
     # pkg prefix
     control_pkg_prefix = get_package_share_directory("erasers_kachaka_control")
+    common_pkg_prefix = get_package_share_directory("erasers_kachaka_common")
+    rviz_name = "erasers_kachaka.rviz"
+    rviz_prefix = os.path.join(common_pkg_prefix, "config", rviz_name)
+
+    # declare arguments
+    declare_provide_map = DeclareLaunchArgument("provide_map",
+                                                default_value="true",
+                                                description="Whether to deliver a map frame from the host")
+    declare_show_rviz = DeclareLaunchArgument("show_rviz",
+                                                default_value="false",
+                                                description="Whether to display Rviz2")
 
     # include launch
     teleop_launch = IncludeLaunchDescription(
@@ -58,6 +77,14 @@ def generate_launch_description():
         package="tf2_ros",
         executable="static_transform_publisher",
         arguments=map2odom_pose,
+        condition=IfCondition(LaunchConfiguration('provide_map')),
+    )
+
+    rviz = Node(
+        package = "rviz2",
+        executable = "rviz2",
+        condition=IfCondition(LaunchConfiguration('show_rviz')),
+        arguments=["-d", rviz_prefix]
     )
 
     # launch message
@@ -70,6 +97,8 @@ def generate_launch_description():
     )
 
     # add actions
+    ld.add_action(declare_provide_map)
+    ld.add_action(declare_show_rviz)
     ld.add_action(teleop_launch)
     ld.add_action(service_tts)
     ld.add_action(emergency_manager)
@@ -77,8 +106,9 @@ def generate_launch_description():
     ld.add_action(docking_manager)
     ld.add_action(battery_manager)
     ld.add_action(sound_manager)
-    #ld.add_action(map2odom)
+    ld.add_action(map2odom)
     ld.add_action(startup_sound_command)
+    ld.add_action(rviz)
 
     # include and execute
     return ld
