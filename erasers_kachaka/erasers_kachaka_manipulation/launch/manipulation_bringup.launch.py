9f9e8a6d5f5c8f8d4baa3491cfc613bce7e3101b
GAI-313 <nakatogawagai@gmail.com>
2024-12-05 18:21:42 +0900

    Add declare sound

--- erasers_kachaka/erasers_kachaka_manipulation/launch/manipulation_bringup.launch.py
+++ erasers_kachaka/erasers_kachaka_manipulation/launch/manipulation_bringup.launch.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, ExecuteProcess
 from launch.substitutions import LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from ament_index_python.packages import get_package_share_directory
 import os
+
+NAMESPACE = "/er_kachaka"
+
+# 起動時の発話メッセージを指定します。黙らせたい場合は空の文字列
+DECLARE_BOOT_MESSAGE = "マニピュレーター、スタート！"
 
 def generate_launch_description():
     ld = LaunchDescription()
@@ -31,5 +36,17 @@ def generate_launch_description():
     )
 
     ld.add_action(manipulation_launch)
+
+    # launch message
+    startup_sound_command = ExecuteProcess(
+        cmd=[
+            'ros2 service call %s/tts erasers_kachaka_interfaces/srv/Speaker "{text: %s}"'%(NAMESPACE, DECLARE_BOOT_MESSAGE)
+        ],
+        shell=True,
+        output="own_log",
+    )
+
+    # load actions
+    ld.add_action(startup_sound_command)
 
     return ld
