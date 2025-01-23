c9749e2789a0712b536abd28d79ba0ef2b5d2d9a
GAI-313 <nakatogawagai@gmail.com>
2025-01-23 19:09:39 +0900

    Add bringup message

--- erasers_kachaka/erasers_kachaka_bringup/launch/bringup.launch.py
+++ erasers_kachaka/erasers_kachaka_bringup/launch/bringup.launch.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess, IncludeLaunchDescription
+from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler
+from launch_ros.actions import Node
+from launch.event_handlers import OnProcessStart
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from ament_index_python.packages import get_package_share_directory
@@ -9,12 +11,27 @@ import os
 
 KACHAKA_NAME = os.environ.get('KACHAKA_NAME')
 
+BRINGUP_MSG = "Kachaka スタート！"
+
 
 def generate_launch_description():
     ld = LaunchDescription()
 
+
     prefix_erk_teleop = get_package_share_directory("erasers_kachaka_teleop")
 
+
+    # NODES
+    node_kachaka_speak_subscriber = Node(
+        package="erasers_kachaka_common",
+        executable="kachaka_speak_subscriber",
+        namespace=KACHAKA_NAME
+    )
+
+    ld.add_action(node_kachaka_speak_subscriber)
+
+
+    # PROCESS
     bringup_docker = ExecuteProcess(
         cmd=[[
             "docker compose",
@@ -23,10 +40,29 @@ def generate_launch_description():
         ]],
         shell=True
     )
+    bringup_msg = RegisterEventHandler(
+        OnProcessStart(
+            target_action=node_kachaka_speak_subscriber,
+            on_start=[
+                ExecuteProcess(
+                    cmd=[[
+                        "ros2 topic pub --once",
+                        " /%s/kachaka_speak"%KACHAKA_NAME,
+                        " std_msgs/msg/String",
+                        " \"{data: %s}\""%BRINGUP_MSG
+                    ]],
+                    shell=True
+                )
+            ]
+        )
+    )
+
 
     ld.add_action(bringup_docker)
+    ld.add_action(bringup_msg)
 
 
+    # LAUNCHERS
     launch_teleop = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([
             prefix_erk_teleop,
