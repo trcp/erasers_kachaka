86e0901e7c04096877c08df382b05f3c36bc56a5
GAI-313 <nakatogawagai@gmail.com>
2025-01-31 21:37:33 +0900

    Add callback

--- erasers_kachaka/erasers_kachaka_common/erasers_kachaka_common/tts.py
+++ erasers_kachaka/erasers_kachaka_common/erasers_kachaka_common/tts.py
@@ -19,7 +19,7 @@ class TTS():
         self.__action_client = ActionClient(self.__node, ExecKachakaCommand, "/%s/kachaka_command/execute"%NS)
 
         if not self.__action_client.wait_for_server(timeout_sec=wait_time):
-            self.__node.get_logger().fatail("May be KACHAKA is not running ...")
+            self.__node.get_logger().fatal("May be KACHAKA is not running ...")
         
         # create command and action field
         self.__cmd = KachakaCommand()
@@ -27,12 +27,18 @@ class TTS():
 
         self.__goal_msg = ExecKachakaCommand.Goal()
     
+    def __cb_response(self, future):
+        goal_handle = future.result()
+        result_future = goal_handle.get_result_async()
+        print(result_future)
+
 
     def say(self, text:str, wait=True):
         self.__cmd.speak_command_text = text
         self.__goal_msg.kachaka_command = self.__cmd
         
         future = self.__action_client.send_goal_async(self.__goal_msg)
+        future.add_done_callback(self.__cb_response)
         
         if wait:
             rclpy.spin_until_future_complete(self.__node, future)
