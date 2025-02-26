bc4699f6a8a3c7ecec4ac522ed7af54dff4e8afc
GAI-313 <nakatogawagai@gmail.com>
2025-02-26 11:42:08 +0900

    Add rviz

--- erasers_kachaka/erasers_kachaka_cartographer/launch/cartographer_launch.py
+++ erasers_kachaka/erasers_kachaka_cartographer/launch/cartographer_launch.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
@@ -22,6 +24,9 @@ def generate_launch_description():
         get_package_share_directory("erasers_kachaka_cartographer"),"config"
     )
     default_config_filename = "cartographer.lua"
+    default_rviz = os.path.join(
+        default_config_dir, "cartographer.rviz"
+    )
 
 
     # configs
@@ -29,6 +34,8 @@ def generate_launch_description():
     config_map_dir = LaunchConfiguration("map_dir")
     config_map_name = LaunchConfiguration("map_name")
     config_map_save_late = LaunchConfiguration("map_save_late")
+    config_use_rviz = LaunchConfiguration("use_rviz")
+    config_use_navigation = LaunchConfiguration("use_navigation")
     config_config_dir = LaunchConfiguration("config_dir")
     config_config_filename = LaunchConfiguration("config_name")
     config_resolution = LaunchConfiguration('resolution')
@@ -42,6 +49,14 @@ def generate_launch_description():
     declare_map_name = DeclareLaunchArgument(
         'map_name', default_value=default_map_name,
         description="保存されるマップ名を指定します。"
+    )
+    declare_use_rviz = DeclareLaunchArgument(
+        'use_rviz', default_value="True",
+        description="Rviz2 を起動します。"
+    )
+    declare_use_navigation = DeclareLaunchArgument(
+        'use_navigation', default_value="True",
+        description="Navigation を有効にします。"
     )
     declare_map_save_late = DeclareLaunchArgument(
         'map_save_late', default_value=defaulr_map_save_late,
@@ -67,10 +82,29 @@ def generate_launch_description():
     ld.add_action(declare_map_dir)
     ld.add_action(declare_map_name)
     ld.add_action(declare_map_save_late)
+    ld.add_action(declare_use_rviz)
+    ld.add_action(declare_use_navigation)
     ld.add_action(declare_config_dir)
     ld.add_action(declare_config_name)
     ld.add_action(declare_resolution)
     ld.add_action(declare_publish_period_sec)
+
+
+    # navigation
+    launch_navigation = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(
+                get_package_share_directory('erasers_kachaka_navigation'),
+                'launch', 'navigation_launch.py'
+            )
+        ]),
+        launch_arguments={
+            'use_map': 'False'
+        }.items(),
+        condition=IfCondition(config_use_navigation)
+    )
+    
+    ld.add_action(launch_navigation)
 
 
     node_cartographer = Node(
@@ -109,10 +143,17 @@ def generate_launch_description():
             {'save_late': config_map_save_late}
         ]
     )
+    node_rviz = Node(
+        package="rviz2",
+        executable="rviz2",
+        arguments=["-d", default_rviz],
+        condition=IfCondition(config_use_rviz)
+    )
     
     ld.add_action(node_cartographer)
     ld.add_action(node_occupancy_grid_node)
     ld.add_action(node_map_saver)
+    ld.add_action(node_rviz)
 
 
     return ld
