33395c9f9187d0648a97b10c8ed18b1ffdb8c42d
GAI-313 <nakatogawagai@gmail.com>
2025-03-25 09:51:57 +0900

    Add bringup service

--- bringup.bash
+++ bringup.bash
@@ -1,5 +1,13 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-source /home/roboworks/colcon_ws/install/setup.bash
+echo "Bringup Type: ${TYPE_BRINGUP}"
+echo "Shelf Type: ${TYPE_SHELF}"
 
-ros2 launch erasers_kachaka_bringup bringup.launch.py bringup_type:=${TYPE_BRINGUP} shelf_type:=${TYPE_SHELF}
+export API_GRPC_BRIDGE_SERVER_URI="${KACHAKA_IP}:${GRPC_PORT}"
+
+#source /home/roboworks/colcon_ws/install/setup.bash
+. /home/roboworks/colcon_ws/install/setup.bash
+
+#ros2 launch erasers_kachaka_bringup bringup.launch.py bringup_type:=${TYPE_BRINGUP} shelf_type:=${TYPE_SHELF}
+ros2 launch erasers_kachaka_bringup bringup.launch.py
+#while true; do  echo "Shelf Type: ${TYPE_SHELF}"; sleep 1s; done
