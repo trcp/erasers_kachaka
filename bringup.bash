7d1dd57c8707a9da688539ce99412d7c9f2a1460
GAI-313 <nakatogawagai@gmail.com>
2025-04-24 14:55:24 +0900

    update automatic bringup scripts

--- bringup.bash
+++ bringup.bash
@@ -8,6 +8,6 @@ export API_GRPC_BRIDGE_SERVER_URI="${KACHAKA_IP}:${GRPC_PORT}"
 #source /home/roboworks/colcon_ws/install/setup.bash
 . /home/roboworks/colcon_ws/install/setup.bash
 
-#ros2 launch erasers_kachaka_bringup bringup.launch.py bringup_type:=${TYPE_BRINGUP} shelf_type:=${TYPE_SHELF}
-ros2 launch erasers_kachaka_bringup bringup.launch.py
+ros2 launch erasers_kachaka_bringup bringup.launch.py bringup_type:=${TYPE_BRINGUP} shelf_type:=${TYPE_SHELF}
+#ros2 launch erasers_kachaka_bringup bringup.launch.py
 #while true; do  echo "Shelf Type: ${TYPE_SHELF}"; sleep 1s; done
