b52573a684f0d40f0a30fcc27b55bdf260ba25e0
GAI-313 <nakatogawagai@gmail.com>
2025-06-03 16:12:21 +0900

    fix command bug

--- bringup.bash
+++ bringup.bash
@@ -2,6 +2,8 @@
 
 echo "Bringup Type: ${TYPE_BRINGUP}"
 echo "Shelf Type: ${TYPE_SHELF}"
+echo "Use Navigation: ${USE_NAVIGATION}"
+echo "Map Path: ${MAP_PATTH}"
 
 export API_GRPC_BRIDGE_SERVER_URI="${KACHAKA_IP}:${GRPC_PORT}"
 
@@ -9,5 +11,5 @@ export API_GRPC_BRIDGE_SERVER_URI="${KACHAKA_IP}:${GRPC_PORT}"
 . /home/roboworks/colcon_ws/install/setup.bash
 
 ros2 launch erasers_kachaka_bringup bringup.launch.py bringup_type:=${TYPE_BRINGUP} shelf_type:=${TYPE_SHELF}
-#ros2 launch erasers_kachaka_bringup bringup.launch.py
-#while true; do  echo "Shelf Type: ${TYPE_SHELF}"; sleep 1s; done
+
+
