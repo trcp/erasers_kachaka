3a2cab6e6ab5370670228d9976755fecbf78e61a
GAI-313 <nakatogawagai@gmail.com>
2025-06-15 10:05:40 +0900

    Add NAVIGATION param

--- navigation.bash
+++ navigation.bash
@@ -2,7 +2,8 @@
 
 echo "Map Path: ${MAP_PATTH}"
 
-
 . /home/roboworks/colcon_ws/install/setup.bash
 
-ros2 launch erasers_kachaka_navigation navigation_launch.py map:=${MAP_PATH}
+if [ "$NAVIGATION" = "True" ]; then
+  ros2 launch erasers_kachaka_navigation navigation_launch.py map:=${MAP_PATH}
+fi
