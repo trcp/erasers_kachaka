8b250c77443b80dc964d4b62e0a35bbf8101f09c
GAI-313 <nakatogawagai@gmail.com>
2026-04-05 18:51:51 +0900

    カチャカシェルフに対応

--- erasers_kachaka/erasers_kachaka_description/README.md
+++ erasers_kachaka/erasers_kachaka_description/README.md
@@ -1,5 +1,26 @@
 # erasers_kachaka_description
-　erasers_kachaka のロボットモデルをスポーンさせるパッケージです。
+　erasers_kachaka のロボットモデルをスポーンさせるパッケージです．以下の２つのオプションモデルを提供します．
+
+|Kachaka 単体|Kachaka + ２段シェルフ|Kachaka + ３段シェルフ|
+|:---:|:---:|:---:|
+|<img src="https://i.imgur.com/GqBfB6Q.png"/>|<img src="https://i.imgur.com/9MUNfMY.png"/>|<img src="https://i.imgur.com/7ds35UQ.png"/>|
+
+## 使用方法
+`debug` 引数を有効にすることでロボットモデルを確認することができます．
+
+- kachaka 単体
+    ```bash
+    ros2 launch erasers_kachaka_description description.launch.py debug:=true use_shelf:=false
+    ```
+- kachaka + ２段シェルフ
+    ```bash
+    ros2 launch erasers_kachaka_description description.launch.py debug:=true use_shelf:=true
+    ```
+- kachaka + ２段シェルフ
+    ```bash
+    ros2 launch erasers_kachaka_description description.launch.py debug:=true use_shelf:=true shelf_type:=3
+    ```
+
 
 # Build
 ```
--- erasers_kachaka/erasers_kachaka_description/launch/description.launch.py
+++ erasers_kachaka/erasers_kachaka_description/launch/description.launch.py
@@ -2,6 +2,7 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration, Command
+from launch.conditions import IfCondition
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import xacro
@@ -11,13 +12,23 @@ import os
 def generate_launch_description():
     ld = LaunchDescription()
 
-    # plefix
-    erasers_kachaka_description = get_package_share_directory('erasers_kachaka_description')
+    # default value
+    default_robot_description = os.path.join(
+        get_package_share_directory('erasers_kachaka_description'),
+        'urdf', 'kachaka.urdf.xacro'
+    )
+    default_rviz_path = os.path.join(
+        get_package_share_directory('erasers_kachaka_description'),
+        'rviz', 'description.rviz'
+    )
 
 
     # configures
     namespace = LaunchConfiguration('namespace')
+    robot_description = LaunchConfiguration('robot_description')
     use_shelf = LaunchConfiguration('use_shelf')
+    shelf_type = LaunchConfiguration('shelf_type')
+    debug = LaunchConfiguration('debug')
     use_sim_time = LaunchConfiguration('use_sim_time')
 
 
@@ -26,9 +37,21 @@ def generate_launch_description():
         'namespace', default_value='er_kachaka',
         description='robot namespace'
     )
+    declare_robot_descriptione = DeclareLaunchArgument(
+        'robot_description', default_value=default_robot_description,
+        description='Foll path for robot description'
+    )
     declare_use_shelf = DeclareLaunchArgument(
         'use_shelf', default_value='false',
         description='Docking shelf'
+    )
+    declare_shelf_type = DeclareLaunchArgument(
+        'shelf_type', default_value='2',
+        description='Value of kachaka shelf layer'
+    )
+    declare_debug = DeclareLaunchArgument(
+        'debug', default_value='false',
+        description='Show robot model only'
     )
     declare_use_sim_time = DeclareLaunchArgument(
         'use_sim_time', default_value='false',
@@ -36,13 +59,16 @@ def generate_launch_description():
     )
 
     ld.add_action(declare_namespace)
+    ld.add_action(declare_robot_descriptione)
     ld.add_action(declare_use_shelf)
+    ld.add_action(declare_shelf_type)
+    ld.add_action(declare_debug)
     ld.add_action(declare_use_sim_time)
 
 
     # Xacro -> URDF
     xacro_file = os.path.join(
-        erasers_kachaka_description, 'urdf', 'erasers_kachaka.urdf.xacro'
+        default_robot_description
     )
 
 
@@ -58,7 +84,8 @@ def generate_launch_description():
             {
                 'robot_description': Command([
                     'xacro ', xacro_file, ' ',
-                    'use_shelf:=', use_shelf
+                    'use_shelf:=', use_shelf, ' ',
+                    'shelf_type:=', shelf_type
                 ]),
                 'use_sim_time': use_sim_time
             }
@@ -71,9 +98,17 @@ def generate_launch_description():
         output='screen',
         emulate_tty=True,
     )
+    rviz2 = Node(
+        package='rviz2',
+        executable='rviz2',
+        arguments=['-d', default_rviz_path],
+        emulate_tty=True,
+        condition=IfCondition(debug)
+    )
 
     ld.add_action(robot_state_publisher)
     ld.add_action(joint_state_publisher)
+    ld.add_action(rviz2)
 
 
     return ld
